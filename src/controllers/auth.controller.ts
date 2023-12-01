54e9844a4314326489d470eab684faffe96ddc93
Jem Suarez <jempoolsmz@gmail.com>
2023-12-01 18:50:50 -0500

    Refactoring auth.controller module

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -15,58 +15,59 @@ import { User } from "../interfaces/user";
 module.exports = (router) => {
   router.post("/auth/signup", async (ctx, next) => {
     const _user: User = ctx.request.body;
-
     if (await authServices.getUserByEmail(_user.email)) {
-      const err = new Error(
-        `The email ${_user.email} is already associated with an account`
-      );
       ctx.status = 400;
-      ctx.body = { message: err.message };
-    } else {
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hash = bcrypt.hashSync(_user.password, salt);
-      _user.password = hash;
-      const newUser = await authServices.createUser(_user);
-
-      const user = {
-        name: newUser.name,
-        email: newUser.email,
+      ctx.body = {
+        message: `The email ${_user.email} is already associated with an account`,
       };
-      const accessToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-        expiresIn: ACCESS_TOKEN_EXPIRES_IN,
-      });
-      const refreshToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-        expiresIn: REFRESH_TOKEN_EXPIRES_IN,
-      });
-      ctx.body = { user, accessToken, refreshToken };
+    } else {
+      const newUser = await createUser(_user);
+      const user = { name: newUser.name, email: newUser.email };
+      const tokens = generateTokens(user);
+      ctx.body = { user, ...tokens };
     }
-
     await next();
   });
 
   router.post("/auth/login", async (ctx, next) => {
     const _user: User = ctx.request.body;
-    const existingUser = await authServices.getUserByEmail(_user.email);
-    if (
-      !existingUser ||
-      !bcrypt.compareSync(_user.password, existingUser.password)
-    ) {
-      const err = new Error(`Incorrect email or password.`);
+    try {
+      const existingUser = await verifyUser(_user);
+      const user = { name: existingUser.name, email: existingUser.email };
+      const tokens = generateTokens(user);
+      ctx.body = { user, ...tokens };
+    } catch (err) {
       ctx.status = 400;
       ctx.body = { message: err.message };
-    } else {
-      const user = {
-        name: existingUser.name,
-        email: existingUser.email,
-      };
-      const accessToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-        expiresIn: ACCESS_TOKEN_EXPIRES_IN,
-      });
-      const refreshToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-        expiresIn: REFRESH_TOKEN_EXPIRES_IN,
-      });
-      ctx.body = { user, accessToken, refreshToken };
     }
     await next();
   });
+};
+
+const verifyUser = async (_user: User) => {
+  const existingUser = await authServices.getUserByEmail(_user.email);
+  if (
+    !existingUser ||
+    !bcrypt.compareSync(_user.password, existingUser.password)
+  ) {
+    throw new Error(`Incorrect email or password.`);
+  }
+  return existingUser;
+};
+
+const createUser = async (_user: User) => {
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(_user.password, salt);
+  _user.password = hash;
+  return await authServices.createUser(_user);
+};
+
+const generateTokens = (user: { name: string; email: string }) => {
+  const accessToken = jwt.sign({ user }, process.env.JWT_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRES_IN,
+  });
+  const refreshToken = jwt.sign({ user }, process.env.JWT_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRES_IN,
+  });
+  return { accessToken, refreshToken };
 };
